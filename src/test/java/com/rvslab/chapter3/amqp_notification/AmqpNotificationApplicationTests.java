a576e4865e8ddda5e9b0d806484e7f4597b72371
Woo <leo9634@naver.com>
2021-05-28 03:13:38 -0400

    Initial commit

---
+++ src/test/java/com/rvslab/chapter3/amqp_notification/AmqpNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rvslab.chapter3.amqp_notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmqpNotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
